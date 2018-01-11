decd80ee6d366edee1f564347e26aca32438f950
Edgar Ramirez <tanotuff12@gmail.com>
2018-01-11 12:09:27 -0600

    test atom git

--- webpack.config.js
+++ webpack.config.js
@@ -122,6 +122,14 @@ module.exports = {
         "loader": "raw-loader"
       },
       {
+          test: /\.pug/,
+          loaders: ['html-loader', 'pug-html-loader'],
+          options: {
+            // options to pass to the compiler same as: https://pugjs.org/api/reference.html
+            data: {} // set of data to pass to the pug render.
+          }
+      },
+      {
         "test": /\.(eot|svg|cur)$/,
         "loader": "file-loader",
         "options": {
